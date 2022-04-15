2cf24594242bccca7d9597d3790e2c7ca0449109
Fusn126 <fushen@utry.cn>
2022-04-15 11:03:12 +0800

    feat: 新增了redux的实验项目。

---
+++ redux-quickstart-tutorial/src/components/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import AddTodo from "./AddTodo";
+import TodoList from "./TodoList";
+import Footer from "./Footer";
+
+import { connect } from "react-redux";
+
+
+export const VisibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE"
+};
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos;
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(t => t.completed);
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(t => !t.completed);
+    default:
+      throw new Error("Unknown filter: " + filter);
+  }
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleTodo = this.toggleTodo.bind(this);
+    this.setVisibilityFilter = this.setVisibilityFilter.bind(this);
+  }
+
+  toggleTodo(id) {
+    const { todos } = this.state;
+
+    this.setState({
+      todos: todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    });
+  }
+
+
+  setVisibilityFilter(filter) {
+    this.setState({
+      filter: filter
+    });
+  }
+
+  render() {
+    const { todos, filter } = this.props;
+
+    return (
+      <div>
+        <AddTodo />
+        <TodoList
+          todos={getVisibleTodos(todos, filter)}
+          toggleTodo={this.toggleTodo}
+        />
+        <Footer
+          filter={filter}
+          setVisibilityFilter={this.setVisibilityFilter}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, props) => ({
+  todos: state.todos,
+  filter: state.filter
+});
+  
+export default connect(mapStateToProps)(App);
