dcf966d52bd36d158b9cb0661a75acc84b871019
Fusn126 <fushen@utry.cn>
2022-04-20 16:51:34 +0800

    feat: 去除了proTypes，太老了，多少年前的东西了。现在想规定类型，请直接用typescript。

--- redux-quickstart-tutorial/src/components/Link.js
+++ redux-quickstart-tutorial/src/components/Link.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Link = ({ active, children, onClick }) => (
   <button
@@ -13,10 +12,5 @@ const Link = ({ active, children, onClick }) => (
   </button>
 );
 
-Link.propTypes = {
-  active: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired
-};
 
 export default Link;
--- redux-quickstart-tutorial/src/components/TodoList.js
+++ redux-quickstart-tutorial/src/components/TodoList.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Todo from './Todo'
 
 
@@ -10,16 +9,5 @@ const TodoList = ({ todos, toggleTodo }) => (
     ))}
   </ul>
 )
-
-TodoList.propTypes = {
-  todos: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      completed: PropTypes.bool.isRequired,
-      text: PropTypes.string.isRequired,
-    }).isRequired
-    ).isRequired,
-    toggleTodo: PropTypes.func.isRequired
-  };
 
 export default TodoList;
